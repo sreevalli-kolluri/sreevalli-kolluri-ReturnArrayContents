ed88015d385e6f691b8fa203f07c84993a740541
sreevalli kolluri <valli.kolluri@gmail.com>
2023-03-08 14:15:53 +0000

    Completed Return Array Contents

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -30,6 +30,11 @@ public class Lab {
      * @return a string representation of all values of nums, with the values separated by spaces.
      */
     public String arrayToString(int[] nums){
-        return "";
+        String str=""+nums[0];
+        for(int i = 1; i < nums.length; i++){
+           str=  str  + " " + nums[i];
+        }
+      
+         return str ;
     }
 }
